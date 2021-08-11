dba1399e762a4836e6f308c1505883615bb26477
tarachom <tarachom@i.ua>
2021-08-11 20:19:04 +0300

    up

--- HomeFinances/ConfigurationSelectionForm.cs
+++ HomeFinances/ConfigurationSelectionForm.cs
@@ -308,4 +308,50 @@ namespace HomeFinances
 			}
 		}
 	}
+
+	/// <summary>
+	/// Елемент списку баз даних користувача
+	/// </summary>
+	public class ConfigurationParam
+	{
+		public ConfigurationParam()
+		{
+			DataBaseServer = "localhost";
+			DataBaseLogin = "postgres";
+			DataBasePort = 5432;
+		}
+
+		public string ConfigurationKey { get; set; }
+
+		public string ConfigurationName { get; set; }
+
+		public string DataBaseServer { get; set; }
+
+		public int DataBasePort { get; set; }
+
+		public string DataBaseLogin { get; set; }
+
+		public string DataBasePassword { get; set; }
+
+		public string DataBaseBaseName { get; set; }
+
+		public override string ToString()
+		{
+			return String.IsNullOrWhiteSpace(ConfigurationName) ? "<>" : ConfigurationName;
+		}
+
+		public ConfigurationParam Clone()
+		{
+			ConfigurationParam configurationParam = new ConfigurationParam();
+			configurationParam.ConfigurationKey = Guid.NewGuid().ToString();
+			configurationParam.ConfigurationName = "Копія - " + ConfigurationName;
+			configurationParam.DataBaseServer = DataBaseServer;
+			configurationParam.DataBaseLogin = DataBaseLogin;
+			configurationParam.DataBasePassword = DataBasePassword;
+			configurationParam.DataBaseBaseName = DataBaseBaseName;
+			configurationParam.DataBasePort = DataBasePort;
+
+			return configurationParam;
+		}
+	}
 }
