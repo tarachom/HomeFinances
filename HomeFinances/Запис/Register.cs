0c3697adc564c74a1bf6dd2f0df0793a67e6c8e6
tarachom <tarachom@i.ua>
2021-09-04 10:16:12 +0300

    up

---
+++ HomeFinances/Запис/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeFinances_1_0.РегістриНакопичення.Проводки
+{
+    class Register
+    {
+        static void WriteRegisterAccumulation()
+        {
+
+        }
+    }
+}
